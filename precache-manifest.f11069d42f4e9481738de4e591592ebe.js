2cd1fe6372b5485f91298235fe4e91ec88cea37d
flashdin <flashdin007@gmail.com>
2020-07-11 11:29:33 +0700

    Updates

---
+++ precache-manifest.f11069d42f4e9481738de4e591592ebe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb9d5d7512f8cc1de46a32855e72c37",
+    "url": "/flashdin-app/index.html"
+  },
+  {
+    "revision": "7a3bff7d5f138809f980",
+    "url": "/flashdin-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb5aefcffd2faeb59fd6",
+    "url": "/flashdin-app/static/js/2.47f5b97b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flashdin-app/static/js/2.47f5b97b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a3bff7d5f138809f980",
+    "url": "/flashdin-app/static/js/main.86700718.chunk.js"
+  },
+  {
+    "revision": "8460eb8197ec21169496",
+    "url": "/flashdin-app/static/js/runtime-main.aff19f6f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/flashdin-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
